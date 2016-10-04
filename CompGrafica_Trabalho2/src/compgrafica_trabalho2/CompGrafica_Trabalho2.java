d01b966edff72b03a3447c862db0d74acccac377
LeonardoRez <leonardorec1@gmail.com>
2016-10-04 18:34:26 -0300

    inicio do trabalho 3

--- CompGrafica_Trabalho2/src/compgrafica_trabalho2/CompGrafica_Trabalho2.java
+++ CompGrafica_Trabalho2/src/compgrafica_trabalho2/CompGrafica_Trabalho2.java
@@ -5,17 +5,10 @@ e Algoritmo recursivo de pintura.
  */
 package compgrafica_trabalho2;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
